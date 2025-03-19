8e57c632b483bfc5d2e5f46f3440cfcd9fb61ff2
BiQbicle <blenderplanet65@gmail.com>
2025-03-19 16:43:19 +0500

    Initial commit

---
+++ target/debug/deps/nigerian_login-adf48d23c9bd4934.d
@@ -0,0 +1,5 @@
+/run/media/biqbicle/Secondary/c-stuff/Programming_stuff/Lala_projects/Rust/nigerianballs_login/target/debug/deps/libnigerian_login-adf48d23c9bd4934.rmeta: src/main.rs
+
+/run/media/biqbicle/Secondary/c-stuff/Programming_stuff/Lala_projects/Rust/nigerianballs_login/target/debug/deps/nigerian_login-adf48d23c9bd4934.d: src/main.rs
+
+src/main.rs:
